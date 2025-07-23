468c452ff7286d0b7b8f83c8142b3e31674ad9d3
Dumpling1902 <rmiadamaris@gmail.com>
2025-07-23 10:21:25 -0600

    feat: allow CORS from all origins

--- src/main/java/com/tecdesoftware/market/web/controller/ProductController.java
+++ src/main/java/com/tecdesoftware/market/web/controller/ProductController.java
@@ -12,6 +12,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
+
+@CrossOrigin(origins = "*")
 //Le dice a Spring que va a ser el controlador de una API REST
 @RestController
 @RequestMapping("/products")
